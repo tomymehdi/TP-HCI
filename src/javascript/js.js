0b2b0c182cf828d6dc1fb4960fe82c58b512aaf7
luciana <lulirez@gmail.com>
2011-10-15 15:35:43 -0300

    casa

--- src/javascript/js.js
+++ src/javascript/js.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+	
 	$('#CartContainer').click( function(){
         $("#main").load("./html/cart.html #main > *");
 	});
@@ -8,23 +9,26 @@ $(document).ready( function() {
 	});
 
 	var opacity = 1, toOpacity = 0.5, duration = 2500;
+	
 	 $('#CartContainer').css('opacity',opacity).hover(function() {
-	      $(this).animate({
+	   
+	   $(this).animate({
 				opacity: toOpacity,
 			}, 400 );
-	    }, function() {
+			
+	    } , function() {
 	      $(this).animate({
 				opacity: opacity,
 			}, 100 );
 	    }
-	  );
+	);
 
 
 	$('#WishListContainer').click( function(){
         $("#main").load("./html/wishlist.html #main > *");
 	});
 
-	 $('#WishListContainer').css('opacity',opacity).hover(function() {
+	$('#WishListContainer').css('opacity',opacity).hover(function() {
 	      $(this).animate({
 				opacity: toOpacity,
 			}, 400 );
@@ -37,7 +41,7 @@ $(document).ready( function() {
 
 	 $('#WishList2 a').click( function(){
 	        $("#main").load("./html/wishlist.html #main > *");
-		});
+	});
 
 	$('#login_opt').click( function(){
 		$(this).animate({
@@ -74,9 +78,7 @@ $(document).ready( function() {
         reloadhomeScript();    
         
     });
-    
-    
-  
+      
 	$('.footerItem').mouseover( function(){
 		$(this).animate({
     	opacity: 0.5,
@@ -137,7 +139,39 @@ $(document).ready( function() {
 			setTimeout("loadItems(currentCategory, parseInt(currentPage + 1))", 100);
 		}
 	});
+
 });
+
+
+function initMenu(name) {
+	
+
+	
+  loadMenu();
+ 
+  
+	
+  var id= '#'+name+'_sm';
+	
+  $('#menu ul').hide();
+  $(id).show();
+
+  $('#menu li a').click(
+    function() {
+      var checkElement = $(this).next();
+      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+        return false;
+        }
+      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        $('#menu ul:visible').slideUp('normal');
+        checkElement.slideDown('normal');
+        return false;
+        }
+      }
+    );
+ }
+
+
 
 function reloadhomeScript(){
 	if(document.getElementById("homeScript")){
@@ -151,7 +185,82 @@ function reloadhomeScript(){
 	var hh = document.getElementsByTagName('head')[0];
 	hh.appendChild(ss);
 }
+function reloadhomeFunc(){
+	if(document.getElementById("homeFuncScript")){
+		$("#homeFuncScript").remove();
+	}
+	
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/homefunc.js";
+	ss.id = "homeFuncScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+}
 
+function reloadjsScript(){
+	if(document.getElementById("jsScript")){
+		$("#jsScript").remove();
+	}
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/js.js";
+	ss.id = "jsScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+	
+}
+function reloadServiceScript(){
 
+	if(document.getElementById("ServiceScript")){
+		$("#ServiceScript").remove();
+	}
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/services.js";
+	ss.id = "servicesScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+	
+}
+function reloadArticlesScript(name){
+	
+    if(document.getElementById("articlesScript")){
+		$("#articlesScript").remove();
+	}
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/articles.js";
+	ss.id = "articlesScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+	
+	initMenu(name);
+	
+}
 
+function reloadItemsManagerScript(){
+	if(document.getElementById("itemsManagerScript")){
+		$("#itemsManagerScript").remove();
+	}
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/itemsManager.js";
+	ss.id = "itemsManagerScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+}
+
+function reloadregisterScript(){
+	if(document.getElementById("registerScript")){
+		$("#registerScript").remove();
+	}
+	
+	var ss = document.createElement('script');
+	ss.type = 'text/javascript';
+	ss.src = "./javascript/register.js";
+	ss.id = "registerScript";
+	var hh = document.getElementsByTagName('head')[0];
+	hh.appendChild(ss);
+}
    
--- src/javascript/services.js
+++ src/javascript/services.js
@@ -1,21 +1,119 @@
-// GLOBALES, hay que sacarlas
 
-movies = new Category("Movies", 1);
-cds = new Category("CDs", 2);
-books = new Category("Books", 2);
+//PAISES
+
+function CountriesList(){
+	this.countries = new Array();
+}
+CountriesList.prototype.addCountry = function(count){
+	this.countries.push(count);
+}
+function Country(name, number,code){
+	this.name = name;
+	this.number = number;
+	this.code= code;
+	this.states = new Array();
+	
+}
+function State(name,number,code,countryId){
+	this.name=name;
+	this.number=number;
+	this.code=code;
+	this.countryId=countryId;
+}
+Country.prototype.addState = function (state){
+	this.states.push(state);
+}
 
 
-cart = new Cart();
-wishlist = new Cart();
-itemList = new ItemList();
-partialList = new ItemList();
-languageList = new ItemList();
-CategoriesList = new CategoriesList();
 
-euros = new CoinType("0.6", "&euro;");
-dollars = new CoinType("1", "U$S");
-pesos = new CoinType("4", "AR$");
-mains = new Array();
+
+function Language(id, code, name){
+	this.id = id;
+	this.code = code;
+	this.name = name;
+}
+
+function User(name, password, cart){
+	this.name = name;
+	this.password = password;
+	this.cart = cart;
+	this.addresses = new Array();
+}
+
+//MONEDAS
+
+function CoinType(value, string){
+	this.value = value;
+	this.string = string;
+}	
+CoinType.prototype.toString = function(){
+	return this.string;
+}
+
+
+//ITEMS
+
+function ItemList(){
+	this.items = new Array();
+}
+ItemList.prototype.addItem = function(item){
+	this.items.push(item);
+}
+function Item(name, description, price, subcategory, imageSource){
+	this.name = name;
+	this.description = description;
+	this.price = price;
+	this.subcategory = subcategory;
+	this.imageSource = imageSource;
+}
+
+
+//CART
+function Cart(){
+	this.items = new Array();
+}
+Cart.prototype.total = function (){
+	var count = 0;
+	var i = 0;
+	for(i = 0 ; i < this.items.length; i++){
+		count += parseFloat(this.items[i].price);
+	}
+	return count*currentCoinType.value;
+}
+Cart.prototype.addItem = function (item){
+	this.items.push(item);
+}
+Cart.prototype.getItemCount = function() {
+	return this.items.length;
+}
+
+
+
+//CATEGORY
+
+function CategoriesList(){
+	this.categories = new Array();
+}
+CategoriesList.prototype.addCategory = function(cat){
+	this.categories.push(cat);
+}
+function Category(name, number, code){
+	this.name = name;
+	this.number = number;
+	this.code=code;
+	this.subcategories = new Array();
+}
+Category.prototype.add = function (category){
+	this.subcategories.push(category);
+}
+
+function Subcategory(name, category, number){
+	this.name = name;
+	this.category = category;
+	this.number = number;
+	category.add(this);
+}
+
 
 
 // VARIABLES
@@ -25,39 +123,137 @@ var currentCoinType;
 var itemQty;
 var currentCategory;
 var currentLanguage=1;
+var currentCountry;
+
+$(document).ready( function() {
+	
+	
+	// GLOBALES, hay que sacarlas
+
+	movies = new Category("Movies", 1);
+	cds = new Category("CDs", 2);
+	books = new Category("Books", 2);
 
 
-loadMainCategories();
+	cart = new Cart();
+	wishlist = new Cart();
+	itemList = new ItemList();
+	partialList = new ItemList();
+	languageList = new ItemList();
+	CategoriesList = new CategoriesList();
+	CountriesList = new CountriesList();
 
-setTimeout("appendCats()",2000);
-setTimeout("reloadhomeScript()",2000);
+	euros = new CoinType("0.6", "&euro;");
+	dollars = new CoinType("1", "U$S");
+	pesos = new CoinType("4", "AR$");
+	mains = new Array();
 
+	
+	
+	
+	loadCommons();
+	loadMainCategories();
+	
+//	setTimeout("appendCats()",2000);
+	reloadhomeScript();
+	
+});
 
-function Common(basicURL){
-	this.basicURL = basicURL;
+function loadCommons()
+{
+	loadLanguages();
+	loadCountries();
+}	
+function loadCountries(){
+	
+	
+	url='./service/Common.groovy?method=GetCountryList&language_id='+currentLanguage;
+
+	var request;
+	var j=0;
+	var cn;
+	var xx,x,i;
+
+		if (window.XMLHttpRequest)
+		{
+			request=new XMLHttpRequest();
+		}
+		else
+		{
+			request=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+
+		request.onreadystatechange=function(){
+
+			if (request.readyState==4 && request.status==200){
+
+				x=request.responseXML.documentElement.getElementsByTagName("country");
+
+	            for (i=0;i<x.length;i++)
+				{
+					xx = x[i];
+					cn = new Country(xx.getElementsByTagName("name")[0].firstChild.nodeValue ,xx.getAttribute("id"),xx.getElementsByTagName("code")[0].firstChild.nodeValue);
+					CountriesList.addCountry(cn);
+					
+				}
+
+			}
+		}
+
+		request.open("GET",url,true);
+		request.send();
+
+	
+	
+	
 }
+function loadStates(country){
+	
+	
+	url='./service/Common.groovy?method=GetStateList&language_id='+currentLanguage+'&country_id='+country.number;
 
-Common.prototype.getLanguages = function(response){
-	response = new Array();
-	url = this.basicURL + "method=getLanguageList";
-	loadLanguages(url, response);
-	loadLanguages(this.basicURL + "method=GetLanguageList", response);
-	var i = 0;
+	var request;
+	var j=0;
+	var st;
+	var xx,x,i;
+
+		if (window.XMLHttpRequest)
+		{
+			request=new XMLHttpRequest();
+		}
+		else
+		{
+			request=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+
+		request.onreadystatechange=function(){
+
+			if (request.readyState==4 && request.status==200){
+
+				x=request.responseXML.documentElement.getElementsByTagName("state");
+
+	            for (i=0;i<x.length;i++)
+				{
+					xx = x[i];
+					st = new State(xx.getElementsByTagName("name")[0].firstChild.nodeValue ,xx.getAttribute("id"),xx.getElementsByTagName("code")[0].firstChild.nodeValue,xx.getElementsByTagName("country_id")[0].firstChild.nodeValue);
+					country.addState(st);
+					
+				}
+
+			}
+		}
+
+		request.open("GET",url,true);
+		request.send();
+
+	
+	
+	
 }
-
-function Security(basicURL){
-	this.basicURL = basicURL;
-}
-
-function Catalog(basicURL){
-	this.basicURL = basicURL;
-}
-
-function Order(basicURL){
-	this.basicURL = basicURL;
-}
-
-function loadLanguages(url, response){
+function loadLanguages(){
+	
+	url='./service/Common.groovy?method=GetLanguageList';
+	
 	var request;
 	var xx,x,i;
 	if (window.XMLHttpRequest)
@@ -74,8 +270,8 @@ function loadLanguages(url, response){
 			for (i=0;i<x.length;i++)
 			{
 				xx = x[i];
-				var language = new Language(xx.getAttribute("id"),  xx.getElementsByTagName("name")[0].firstChild.nodeValue,  xx.getElementsByTagName("code")[0].firstChild.nodeValue);
-				response.addItem(language);
+				var lang = new Language(xx.getAttribute("id"),  xx.getElementsByTagName("name")[0].firstChild.nodeValue,  xx.getElementsByTagName("code")[0].firstChild.nodeValue);
+				languageList.addItem(lang);
 			}
 		}
 	}
@@ -83,65 +279,49 @@ function loadLanguages(url, response){
 	request.send();
 }
 
-$(document).ready( function() {
-	common = new Common("./service/Common.groovy?");
-	common.getLanguages(languages);
-	myLanguages(languages);
-});
 
-function myLanguages(languages){
-	var i;
-	for(i = 0; i < languages.items.length; i++){
-		alert(languages.items[i]);
-	}
-}
 
 function loadMainCategories(){
-    
-    
-url='./service/Catalog.groovy?method=GetCategoryList&language_id='+currentLanguage;
+	
+	url='./service/Catalog.groovy?method=GetCategoryList&language_id='+currentLanguage;
 
-var request;
-var j=0;
-var cate;
-var xx,x,i;
+	var request;
+	var j=0;
+	var cn;
+	var xx,x,i;
 
-    
-	if (window.XMLHttpRequest)
-	{
-		request=new XMLHttpRequest();
-	}
-	else
-	{
-		request=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-    
-	request.onreadystatechange=function(){
-    
-		if (request.readyState==4 && request.status==200){
-        
-			x=request.responseXML.documentElement.getElementsByTagName("category");
-			
-            for (i=0;i<x.length;i++)
-			{
-				xx = x[i];
-				cat = new Category(xx.getElementsByTagName("name")[0].firstChild.nodeValue, xx.getAttribute("id"));
-				
-				
-				CategoriesList.addCategory(cat);
-				
-				loadSubCategories(cat);
-			}
-			
+		if (window.XMLHttpRequest)
+		{
+			request=new XMLHttpRequest();
 		}
-	}
-    
-	request.open("GET",url,true);
-	request.send();
-        
-    
-}
+		else
+		{
+			request=new ActiveXObject("Microsoft.XMLHTTP");
+		}
 
+		request.onreadystatechange=function(){
+
+			if (request.readyState==4 && request.status==200){
+
+				x=request.responseXML.documentElement.getElementsByTagName("category");
+
+	            for (i=0;i<x.length;i++)
+				{
+					xx = x[i];
+					cn = new Category(xx.getElementsByTagName("name")[0].firstChild.nodeValue ,xx.getAttribute("id"),xx.getElementsByTagName("code")[0].firstChild.nodeValue);
+					CategoriesList.addCategory(cn);
+					loadSubCategories(cn);
+					
+				}
+
+			}
+		}
+
+		request.open("GET",url,true);
+		request.send();
+
+}
+	
 function loadSubCategories(categ){
 	
 	url='./service/Catalog.groovy?method=GetSubcategoryList&language_id='+currentLanguage+'&category_id='+categ.number;
@@ -183,41 +363,24 @@ function loadSubCategories(categ){
 }
 
 
-function appendCats(){    
+function loadMenu(){
 
-var j=0;
-var cate;
-
-$('#main_opt').remove();
-$('#FooterCat').empty();
-
-while(j < CategoriesList.categories.length){
-	cate = CategoriesList.categories[j];
-    $('#main').append('<div id="main_opt"><div  class="'+cate.name+'_link  title" >'+cate.name+'</div>');
-    $('#FooterCat').append('<li><a  class="'+cate.name+'_link pointer">'+cate.name+'</a></li>');
-	j++;
-}
-
-}
-
-/*NO anda, porque no queda guardado categories list
-function loadMenu(catego){
-	
 	var w=0;
 	var e=0;
-	
-	while(w < CategoriesList.categories.length){
-	
+
+	while(w <  CategoriesList.categories.length){
+
 	$('#menu').append('<li><a href="#">'+CategoriesList.categories[w].name+'</a><ul id="'+CategoriesList.categories[w].name+'_sm">');
-	
-	while(e < CategoriesList.categories[w].subcategories.length){	
-	
-	$('#'+CategoriesList.categories[w].name+'_sm').append('<li><a href="#" >'+CategoriesList.categories[w].subcategories.name+'</a></li>');
-	
+
+	while(e <  CategoriesList.categories[w].subcategories.length){	
+
+	$('#menu').append('<li><a href="#" >'+CategoriesList.categories[w].subcategories.name+'</a></li>');
+
 	e++;
 	}
 	e=0;
 	$('#menu').append('</ul></li>');
-		
+
 	}
-} */
+}
+
