22241a05371202992d1bf311142323a8a0d705be
Tomas Mehdi <tomymehdi@gmail.com>
2011-10-15 17:57:54 -0300

    last

--- src/javascript/cart.js
+++ src/javascript/cart.js
@@ -5,23 +5,39 @@ $(document).ready( function() {
 });
 
 function chargeCartItems(){
-	var items = new Array();
+	var cartItems = cart.getItems();
+	var cant = cart.getItemCount();
+	var itemsNoRepeat = new Array();
+	var countEachItem = new Array();
+	var flag = true;
 	$('#ListIt').empty();
-	for(var i = 0; i < cart.getItemCount() ; i++){
-		var flag = true;
-	/*	for(var j = 0; flag && j < cart.getItemCount() ; j++){
-			if(items[j] == cart.getItems[j].name){
+	for(var i = 0; i < cant ; i++){
+		flag = true;
+		for(var j = 0; j < itemsNoRepeat.length ; j++){
+			if(itemsNoRepeat[j] == cartItems[i]){
 				flag = false;
 			}
-		} */
+		}
+		if(flag){
+			itemsNoRepeat.push(cartItems[i]);
+		}
+	}
+	for(var i = 0 ; i < cant ; i++){
+		for(var j = 0; j < itemsNoRepeat.length ; j++){
+			if(cartItems[i] == itemsNoRepeat[j]){
+				countEachItem[j]++;
+			}
+		}
+	}
+	
 		$('#ListIt').append('<div id="OnCartItem">' +
-			'<div id="itemName" onclick="bringInfo(' + i + ')" class="pointer">' 
+			'<div id="OnCartItemItemName" onclick="bringInfo(' + i + ')" class="pointer">' 
 			+ roundString(item.name, 25) + 
-			'</div><img id="Image" class="pointer" onclick="bringInfo(' + 
+			'</div><img id="OnCartItemImage" class="pointer" onclick="bringInfo(' + 
 			i + ')" src="'  + item.imageSource + '"></img>' +
-			'<div id="Options">' +
 			'<select id="sel' + i + '" size="1" class="quantity"> ' +
-			currentCoinType + '</div><div id="PriceNumber" >' + roundNumber(item.price*currentCoinType.value, 2) + 
-			'</div></div></div>' +
-			'</div>');}
+			currentCoinType + '</div>' +
+			'<div id="PriceNumber" >' + roundNumber(item.price*currentCoinType.value, 2) + '</div>' +
+			'</div></div>' +
+			'</div>');
 }
